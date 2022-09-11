447571def17a0f9210524db972a53d756ca623c2
Turq0ise <72288865+Turq0ise@users.noreply.github.com>
2022-09-11 17:36:25 +0800

    09/11/22 progress

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,23 @@ function fixSpacing() {
     getMainSection.style.paddingTop = `${getMainNav.clientHeight}px`
 }
 
+const navListItems = document.querySelectorAll(".nav-li")
+for(let i = 0; i < navListItems.length; i++){
+    let navListItem = navListItems[i]
+    navListItem.onclick = e => {
+        e.srcElement.localName.toLowerCase()
+        console.log(e)
+        
+        if(e.srcElement.localName.toLowerCase() === "li") {
+            console.log(e.srcElement.localName.toLowerCase())
+            //e.srcElement.children[1]
+        } else if(e.srcElement.localName.toLowerCase() === "a") {
+            console.log("bruh trolling")
+            //e.srcElement.nextElementSibling
+        }
+    }
+}
+
 const standardTime = 60
 
 let setHours = 0   //max 99 hours or 5940 mins or 356400 seconds
