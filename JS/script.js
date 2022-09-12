c551f4ef23f5248961a286ae5defa697518db78f
Turq0ise <72288865+Turq0ise@users.noreply.github.com>
2022-09-12 19:08:09 +0800

    09/12/22 progress

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,40 @@
 const getMainNav = document.getElementById("main-nav")
 const getMainSection = document.getElementById("main-section")
 
+window.onload = () => {
+    fixSpacing()
+    addCurrentIndicator()
+}
+
+console.log(window.location)
 function fixSpacing() {
     getMainSection.style.paddingTop = `${getMainNav.clientHeight}px`
 }
 
 const navListItems = document.querySelectorAll(".nav-li")
+let getCurrentIndicator = document.querySelectorAll(".current-indicator")
+let currentSectionId = window.location.hash
+
+function addCurrentIndicator() {
+    if(currentSectionId === "") {
+        getCurrentIndicator[0].style.opacity = "100"
+    } else {
+        document.querySelector(`[href="${currentSectionId}"]`).children[0].children[1].style.opacity = "100"
+    }
+}
+
 for(let i = 0; i < navListItems.length; i++){
     let navListItem = navListItems[i]
     navListItem.onclick = e => {
         e.srcElement.localName.toLowerCase()
-        console.log(e)
+        for(let x = 0; x < getCurrentIndicator.length; x++) {
+            getCurrentIndicator[x].style.opacity = "0"
+        }
         
         if(e.srcElement.localName.toLowerCase() === "li") {
-            console.log(e.srcElement.localName.toLowerCase())
-            //e.srcElement.children[1]
-        } else if(e.srcElement.localName.toLowerCase() === "a") {
-            console.log("bruh trolling")
-            //e.srcElement.nextElementSibling
+            e.srcElement.children[1].style.opacity = "100"
+        } else if(e.srcElement.localName.toLowerCase() === "p") {
+            e.srcElement.nextElementSibling.style.opacity = "100"
         }
     }
 }
